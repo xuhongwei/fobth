e7a726693b26ccef15d07f1f4cc9e9ea37e5bd28
xuhongwei <413486774@qq.com>
2014-03-05 15:47:24 +0800

    modify the pre and next product

--- app/models/product.rb
+++ app/models/product.rb
@@ -21,16 +21,20 @@ class Product < ActiveRecord::Base
   def previous(query)
     unless query.nil?
       index = query.find_index(self.id)
-      prev_id = query[index-1] unless index.zero?
-      self.class.find_by_id(prev_id)
+      if index
+        prev_id = query[index-1] unless index.zero?
+        self.class.find_by_id(prev_id)
+      end
     end
   end
 
   def next(query)
     unless query.nil?
       index = query.find_index(self.id)
-      next_id = query[index+1] unless index == query.size
-      self.class.find_by_id(next_id)
+      if index
+        next_id = query[index+1] unless index == query.size
+        self.class.find_by_id(next_id)
+      end
     end
   end
 end
